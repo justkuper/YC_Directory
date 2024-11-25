83c953c4e721cbbdbf23467e940dc547b5f86f30
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-25 00:38:48 -0800

    changed title to name in validation ts

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-  title: z.string().min(3).max(100),
+  name: z.string().min(3).max(100),
   description: z.string().min(20).max(500),
   category: z.string().min(3).max(20),
   link: z
